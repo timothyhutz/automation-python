5bf96f22396019a8859430558201345a7ca1a83b
Timothy Hutz <timothy.hutz@pearson.com>
2018-01-18 20:10:57 -0700

    removed AUTH Basic requirments as it does funky stuff in python compared to my control app Insomnia...
    Signed-off-by: Timothy Hutz <timothy.hutz@pearson.com>

--- repo_privacy_inspector.py
+++ repo_privacy_inspector.py
@@ -5,16 +5,13 @@ HAS ONLY BEEN VERFIED working with github and bitbucket..."""
 
 import csv
 import requests
-import base64
 
 with open('repo.csv') as csvfile:
 	repo_detail = csv.DictReader(csvfile)
 	for row in repo_detail:
 		try:
 			print(row['Repo'])
-			creds = bytes.decode(base64.b64encode(b'test' + b':'))
 			header = {
-			#	'Authorization': 'Basic ' + '{}'.format(creds),
 				'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/603.3.8 (KHTML, like Gecko)'
 			}
 			url = requests.get(row['Git Endpoint'], headers=header, allow_redirects=True)
