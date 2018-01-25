dc911b68a00f7a167b7e0e0b6b0e07736bba4548
Timothy Hutz <timothy.hutz@pearson.com>
2018-01-25 13:52:59 -0700

    added Dictionary creation and update / adding of results..
    Signed-off-by: Timothy Hutz <timothy.hutz@pearson.com>

--- aws-lambda/github-privacy-inpector.py
+++ aws-lambda/github-privacy-inpector.py
@@ -5,7 +5,7 @@ serverless architecture"""
 import pip
 import json
 import logging
-logger = logging.getLogger()
+logger=logging.getLogger()
 logger.setLevel(logging.INFO)
 try:
 	import github
@@ -14,12 +14,14 @@ except ImportError:
 	logging.info('pygithub was not in python3.6 modules, it was added so you can use it')
 	import github
 except Exception as message:
-	logger.debug(message)
+	logger.error(message)
 
 def main(event, context):
 	repolist = event['repos']
+	reporesults = {}
 	logger.info(print(repolist))
 	git_session = logger.info(github.Github(login_or_token=event['username'], password=event['password']))
 	for repo in repolist:
 		repodata = json.loads(git_session.get_repo(full_name_or_id=repo))
-
+		reporesults[repo]=repodata['']
+	return reporesults
