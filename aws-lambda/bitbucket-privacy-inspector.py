713e7deb86849bf9d385aa68ca050cf1c3540f44
Timothy Hutz <timothy.hutz@pearson.com>
2018-01-30 16:30:16 -0700

    Iterator is set for pageing setup in bitbucket.
    Signed-off-by: Timothy Hutz <timothy.hutz@pearson.com>

--- aws-lambda/bitbucket-privacy-inspector.py
+++ aws-lambda/bitbucket-privacy-inspector.py
@@ -2,12 +2,26 @@
 This inspects the security settings via bitbucket API call for whether a repo is public or not.."""
 
 import logging
+import boto3
 import requests
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
 
-def main(event):
-	logger.info(event['repos'])
-	for repo in event['repos']:
+def main(event, context):
+	#setting the DB info
+	dynamodb = boto3.resource('dynamodb')
+	dbtable = dynamodb.Table('bitbucket_data')
+	##API url build out..
+	api_url = 'https://' + event['domain'] + '/rest/api/1.0/projects/' + event['project_key']
+	#start page for inital data load at 0
+	page_range = [0]
+	## Empty Dict for repos
+	repo_data = {}
+	## Counter for bitbucket's pageing setup.. Grabs the nextPageStart number and adds it to list...
+	for page_num in page_range:
+		call = requests.get(api_url + '/repos?start={}'.format(page_num), auth=(event['username'], event['password'])).json()
+		if call['isLastPage'] is False:
+				page_range.append(call['nextPageStart'])
+		break
 
