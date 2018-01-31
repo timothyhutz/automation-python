9439be2acbe7c44b4ff82cc165566791c433c8b4
Timothy Hutz <timothy.hutz@pearson.com>
2018-01-30 17:57:04 -0700

    Working with DB loading data...
    Signed-off-by: Timothy Hutz <timothy.hutz@pearson.com>

--- aws-lambda/bitbucket-privacy-inspector.py
+++ aws-lambda/bitbucket-privacy-inspector.py
@@ -8,7 +8,7 @@ logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
 
-def main(event, context):
+def main(event):
 	#setting the DB info
 	dynamodb = boto3.resource('dynamodb')
 	dbtable = dynamodb.Table('bitbucket_data')
@@ -21,6 +21,26 @@ def main(event, context):
 	## Counter for bitbucket's pageing setup.. Grabs the nextPageStart number and adds it to list...
 	for page_num in page_range:
 		call = requests.get(api_url + '/repos?start={}'.format(page_num), auth=(event['username'], event['password'])).json()
+		for repo in call['values']:
+			logger.info(repo)
+			##DB lookup check to see if it exist
+			response = dbtable.get_item(
+				Key={
+					'repo': repo['name']
+				}
+			)
+			## if Key error (which means does not exist) then put data in table.
+			try:
+				response['Item']
+			except KeyError:
+				logger.info(repo['name'] + ' not found adding to DB')
+				dbtable.put_item(
+					Item={
+						'repo': repo['name'],
+						'public': bool(repo['public'])
+					}
+				)
+			## If we hit the last page end the loop for appending to pagestart value.
 		if call['isLastPage'] is False:
 				page_range.append(call['nextPageStart'])
 		break
