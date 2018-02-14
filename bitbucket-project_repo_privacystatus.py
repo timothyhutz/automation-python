65a679b57ae287f38aab73f306317a23c0bebe1d
Timothy Hutz <timothy.hutz@pearson.com>
2018-02-14 13:09:25 -0700

    fixed spelling and typos, Fixed DictWriter source for key, value. setup a proper init to make this modular and callable from another program or from command line as source..
    Signed-off-by: Timothy Hutz <timothy.hutz@pearson.com>

--- bitbucket-project_repo_privacystatus.py
+++ bitbucket-project_repo_privacystatus.py
@@ -2,26 +2,29 @@ import requests
 import csv
 import sys
 
-
+# Primary work data class that's callable if used in module format.
 class Repodata(object):
-
+	# initializes variables to zero and sets the proper project URL
 	def __init__(self):
 		self.api_url = 'https://' + domain + '/rest/api/1.0/projects/' + project_key
 		# start page for inital data load at 0
 		self.page_range = [0]
 		self.repodata = {}
 
+# does the pagination calls to retrieve all data for repos including nextpage to the next list.
 	def main(self):
-		# Counter for bitbucket's pageing setup.. Grabs the nextPageStart number and adds it to list...
+		# Counter for bitbucket's paging setup.. Grabs the nextPageStart number and adds it to list...
 		for page_num in self.page_range:
 			call = requests.get(self.api_url + '/repos?start={}'.format(page_num), auth=(username, password)).json()
 			for repo in call['values']:
-				print(repo)
+				# sets only the data I need, repo name and public status
 				self.repodata[repo['name']]=repo['public']
+				# writes out the CVS file
 				with open('data.csv', 'w') as csvfile:
 					fieldnames = ['repo name', 'public']
 					writer = csv.DictWriter(csvfile, fieldnames)
 					writer.writeheader()
+					#Pairs the Dictonary items into seperate namespaces
 					for key, value in self.repodata.items():
 						writer.writerow({'repo name': key, 'public': value})
 
@@ -30,17 +33,16 @@ class Repodata(object):
 				self.page_range.append(call['nextPageStart'])
 			break
 
-
-args = {}
-for item in sys.argv[1:]:
-	key, val = item.split('=')
-	args[key] = val
-
-domain=args['domain']
-project_key = args['project_key']
-username = args['username']
-password = args['password']
+# setup for called as direct source and not as module
 if __name__ == '__main__':
-	start = Repodata()
-	start.main()
+	args = {}
+	for item in sys.argv[1:]:
+		key, val = item.split('=')
+		args[key] = val
+
+	domain = args['domain']
+	project_key = args['project_key']
+	username = args['username']
+	password = args['password']
+	Repodata().main()
 
