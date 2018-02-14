91761d144c81d6007aababb1f6a275c536a48265
Timothy Hutz <timothy.hutz@pearson.com>
2018-02-13 20:52:33 -0700

    Running code; sys.argv dictonary building and setting token / org.
    Signed-off-by: Timothy Hutz <timothy.hutz@pearson.com>

--- github-orgpublicstatus.py
+++ github-orgpublicstatus.py
@@ -1,21 +1,37 @@
 """written by Timothy Hutz timothyhutz@gmail.com
-Purpose of this pyhton script is to use the github API to check public status of repos.."""
+Purpose of this python script is to use the github API to check public status of repos.."""
 
 import github
+import sys
 
-def main(repos, token):
+# This is the main that loads each repo private status True or False Boolen
+def main(repolist):
 	reporesults = {}
-	for repo in repos:
+	for repo in repolist:
 		repobool = git_session.get_repo(full_name_or_id=repo).private
 		reporesults[repo]=repobool
 	return reporesults
 
+#Builds inital list from all repos in the listed Orginization.
+def repolistbuild(org):
+	for data in git_session.get_organization(login=org).get_repos():
+		repolist.append(data.full_name)
 
-token = None
+#preset global params
+args = {}
+for item in sys.argv[1:]:
+	key, val = item.split('=')
+	args[key]=val
+token = args['token']
+org = args['org']
+repolist = []
+
 try:
 	git_session = github.Github(login_or_token=token)
 except Exception as message:
 	print(message)
 	exit(1)
-repodata =
-repos = []
+
+
+repolistbuild(org)
+print(main(repolist))
